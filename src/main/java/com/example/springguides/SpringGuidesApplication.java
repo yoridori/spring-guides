68b5a8d21980af4d8a0479d695ea16a63857f80d
yoridori <midorika809+github@gmail.com>
2021-02-23 23:44:20 +0900

    create rest-service

---
+++ src/main/java/com/example/springguides/SpringGuidesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuidesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springguides/SpringGuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package springguides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
