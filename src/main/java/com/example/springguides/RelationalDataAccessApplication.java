96b300208489ce2dd4569267bc77385dbf255450
yoridori <midorika809@gmail.com>
2021-04-06 17:54:18 +0900

    それぞれをmainメソッドの実行で実行できるよう修正。

--- src/main/java/com/example/springguides/RelationalDataAccessApplication.java
+++ src/main/java/com/example/springguides/RelationalDataAccessApplication.java
@@ -10,13 +10,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.example.springguides.relationaldataaccess.Customer;
 
-@SpringBootApplication
+@EnableAutoConfiguration
 public class RelationalDataAccessApplication implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(RelationalDataAccessApplication.class);
@@ -24,6 +25,10 @@ public class RelationalDataAccessApplication implements CommandLineRunner {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    public static void main(String[] args) {
+        SpringApplication.run(RelationalDataAccessApplication.class, args);
+    }
+    
     @Override
     public void run(String... strings) throws Exception {
 
